4a9e627f59a89e8babfcf48377811b8ccd876079
Patty Chang <patty.chang@ogglobal.net>
2022-12-02 15:10:28 +0800

    Reversed mac status bar - It causes some troubles for API calling for unknown reason

--- HappyTimeMac/HappyTimeMacApp.swift
+++ HappyTimeMac/HappyTimeMacApp.swift
@@ -11,7 +11,7 @@ import Firebase
 @main
 struct HappyTimeMacApp: App {
     
-    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+//    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     
     private struct MenuView: View {
           var body: some View {
@@ -35,30 +35,30 @@ struct HappyTimeMacApp: App {
     }
 }
 
-class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
-    
-    private var statusItem: NSStatusItem!
-    
-    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
-        
-        let contentView = MainView()
-
-        let view = NSHostingView(rootView: contentView)
-
-        // Don't forget to set the frame, otherwise it won't be shown.
-        view.frame = NSRect(x: 0, y: 0, width: 200, height: 200)
-                
-        let menuItem = NSMenuItem()
-        menuItem.view = view
-                
-        let menu = NSMenu()
-        menu.addItem(menuItem)
-                
-        // StatusItem is stored as a class property.
-        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-        self.statusItem?.menu = menu
-//        self.statusItem?.button?.title = "HAPPY"
-        
-        self.statusItem?.button?.image = NSImage(named: "32")
-    }
-}
+//class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
+//
+//    private var statusItem: NSStatusItem!
+//
+//    @MainActor func applicationDidFinishLaunching(_ notification: Notification) {
+//
+//        let contentView = MainView()
+//
+//        let view = NSHostingView(rootView: contentView)
+//
+//        // Don't forget to set the frame, otherwise it won't be shown.
+//        view.frame = NSRect(x: 0, y: 0, width: 200, height: 200)
+//
+//        let menuItem = NSMenuItem()
+//        menuItem.view = view
+//
+//        let menu = NSMenu()
+//        menu.addItem(menuItem)
+//
+//        // StatusItem is stored as a class property.
+//        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+//        self.statusItem?.menu = menu
+////        self.statusItem?.button?.title = "HAPPY"
+//
+//        self.statusItem?.button?.image = NSImage(named: "32")
+//    }
+//}
